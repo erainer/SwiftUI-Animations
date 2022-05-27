68fa1106775de83804018eadc8d4cea5ec5e7c25
erainer <emily.rainer@lextech.com>
2022-05-27 18:32:58 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Emily Rainer on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
